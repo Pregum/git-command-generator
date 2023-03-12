34e56d99b58386fbe1725cd4f5e63ad50326d451
Pregum <pregum.y@gmail.com>
2023-03-12 23:52:01 +0900

    update: checkoutコマンド実行時に選択しているnodeが水色になるように変更

--- src/components/page/Home/Home.tsx
+++ src/components/page/Home/Home.tsx
@@ -15,7 +15,7 @@ import ReactFlow, {
 } from 'reactflow'
 import { DiagramCanvasDrawArea } from '@/components/model/diagramCanvas/DiagramCanvasDrawArea'
 import { useCustomKeybinding as useCustomKeybinding } from '@/components/ui/CustomKeybinding'
-import { useCallback, useState } from 'react'
+import { CSSProperties, useCallback, useState } from 'react'
 import { useToast } from '@chakra-ui/react'
 
 export type Props = React.PropsWithChildren<{}>
@@ -31,8 +31,21 @@ type Branch = {
 let nodeId = 0
 
 const initialNodes: Node[] = [
-  { id: 'i1', position: { x: 0, y: 0 }, data: { label: 'first commit' } },
-  { id: 'i2', position: { x: 0, y: 100 }, data: { label: 'second commit' } },
+  {
+    id: 'i1',
+    position: { x: 0, y: 0 },
+    data: { label: 'first commit' },
+    width: 200,
+  },
+  {
+    id: 'i2',
+    position: { x: 0, y: 100 },
+    data: { label: 'second commit' },
+    width: 200,
+    style: {
+      backgroundColor: 'aqua',
+    },
+  },
 ]
 
 const initialBranches: Branch[] = [
@@ -93,9 +106,15 @@ export const Home: React.FC<Props> = ({ children }) => {
     // const commitRegex = /git\s+commit\s+(?:(?:-m)?\s+)?[\"\'](?<mes>[\w\_\-]+)[\"\']/
     const commitRegex =
       /git\s+commit\s+(?:-m\s+)(?<quote>["'])(?<mes>[\w\_\-\s]+)\k<quote>/
-    const checkoutRegex = /git\s+checkout\s+-b\s+(?<branchName>[\w\_\-]+)/
+    const checkoutNewBranchRegex =
+      /git\s+checkout\s+-b\s+(?<branchName>[\w\_\-]+)/
+    const checkoutRegex = /git\s+checkout\s+(?<branchName>[\w\_\-]+)/
 
-    if (message.match(checkoutRegex)) {
+    if (message.match(checkoutNewBranchRegex)) {
+      const branchName =
+        message.match(checkoutNewBranchRegex)?.groups?.branchName ?? ''
+      checkoutNewBranchAction(branchName)
+    } else if (message.match(checkoutRegex)) {
       const branchName = message.match(checkoutRegex)?.groups?.branchName ?? ''
       checkoutAction(branchName)
     } else if (message.match(commitRegex)) {
@@ -140,18 +159,41 @@ export const Home: React.FC<Props> = ({ children }) => {
     let y = defaultY + nodeId * 100
     if (lastNode) {
       // lastNode = nodes[nodes.length - 1]
-      x = lastNode.position.x + ((currentBranch.no - 1) * ((lastNode.width ?? 0) + 25)) 
+      // x =
+      //   lastNode.position.x +
+      //   (currentBranch.no - 1) * ((lastNode.width ?? 0) + 25)
+      x = (currentBranch.no - 1) * ((lastNode.width ?? 0) + 25)
       y = lastNode.position.y + 100
     }
 
     const id = `${++nodeId}`
-    const newNode = generateNewNode({ id, x, y, label: parsedMessage })
+    const newNode = generateNewNode({
+      id,
+      x,
+      y,
+      label: parsedMessage,
+      style: { backgroundColor: 'aqua' },
+    })
+
+    const rfiNodes = reactFlowInstance.getNodes()
+    const foundPreviousLatestNode = rfiNodes.find(
+      (node) => node.id == latestNode.id
+    )
+    if (foundPreviousLatestNode) {
+      foundPreviousLatestNode.style = {
+        backgroundColor: 'white',
+      }
+    }
+    reactFlowInstance.setNodes(rfiNodes)
 
     reactFlowInstance.addNodes(newNode)
     if (lastNode) {
       connectEdge(lastNode, newNode)
     }
-    reactFlowInstance.fitView({ minZoom: 0.5, nodes: reactFlowInstance.getNodes() })
+    reactFlowInstance.fitView({
+      minZoom: 0.1,
+      nodes: reactFlowInstance.getNodes(),
+    })
 
     setLatestNode(newNode)
     setCurrentBranch((prev) => {
@@ -176,6 +218,60 @@ export const Home: React.FC<Props> = ({ children }) => {
   }
 
   const checkoutAction = (branchName: string) => {
+    if (!branchName.length) {
+      toast({
+        title: 'ブランチ名が空です',
+        status: 'error',
+        isClosable: true,
+      })
+      return
+    }
+
+    const foundBranch = branches.find(
+      (branch) => branch.branchName == branchName
+    )
+    if (!foundBranch) {
+      toast({
+        title: '該当するブランチが存在しません',
+        description: `branchName: ${branchName}`,
+        status: 'error',
+        isClosable: true,
+      })
+      return
+    }
+
+    // 対象のnodeの色を替える
+
+    setCurrentBranch(foundBranch)
+    const rfiNodes = reactFlowInstance.getNodes()
+    const foundPreviousLatestNode = rfiNodes.find(
+      (node) => node.id == latestNode.id
+    )
+    if (foundPreviousLatestNode) {
+      foundPreviousLatestNode.style = {
+        backgroundColor: 'white',
+      }
+    }
+    const foundNextLatestNode = rfiNodes.find(
+      (node) => node.id == foundBranch.currentNodeId
+    )
+    if (foundNextLatestNode) {
+      foundNextLatestNode.style = {
+        backgroundColor: 'aqua',
+      }
+      setLatestNode(foundNextLatestNode)
+    }
+    reactFlowInstance.setNodes(rfiNodes)
+
+    toast({
+      title: 'ブランチをcheckoutしました',
+      description: `ブランチ名: ${branchName}, latestNode: ${foundNextLatestNode?.id}`,
+      status: 'success',
+      isClosable: true,
+    })
+  }
+
+  const checkoutNewBranchAction = (branchName: string) => {
     // 空文字の場合は処理終了
     if (!branchName.length) {
       toast({
@@ -239,11 +335,13 @@ export const Home: React.FC<Props> = ({ children }) => {
     x,
     y,
     label,
+    style,
   }: {
     id?: string
     x: number
     y: number
     label: string
+    style?: CSSProperties
   }) => {
     const newNode: Node<any, string | undefined> = {
       id: id ?? `${++nodeId}`,
@@ -254,7 +352,8 @@ export const Home: React.FC<Props> = ({ children }) => {
       data: {
         label: label,
       },
-      width: 200
+      width: 200,
+      style,
     }
 
     return newNode
