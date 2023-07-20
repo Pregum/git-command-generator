f4fc8f2a03a7ba3e100d95e85c912ef3649d39d7
Pregum <pregum.y@gmail.com>
2023-07-21 01:49:06 +0900

    update: 線をpush先にも追加する処理を実装

--- src/features/home/hooks/usePushAction.ts
+++ src/features/home/hooks/usePushAction.ts
@@ -7,6 +7,8 @@ import useMyNode from './useMyNode'
 import { NodeKinds } from '../types/nodeKinds'
 import { Node } from 'reactflow'
 import createRemoteRepositoryNode from '../utils/createRemoteRepositoryNode'
+import { REmoteRepositoryRegionNode } from '../types/remoteRepositoryRegionNode'
+import { RemoteRepositoryRegion } from '../types/remoteRepositoryRegion'
 
 export default function usePushAction() {
   const toast = useMyToast()
@@ -31,13 +33,58 @@ export default function usePushAction() {
     // とりあえず試しに枠を作成する。
     const foundRemoteRepositoryNode = findRemoteRepositoryRegion(remoteBranch)
     if (!foundRemoteRepositoryNode) {
-      createRemoteRepositoryRegion(remoteBranch)
+      const newRepositoryNode = createRemoteRepositoryRegion(remoteBranch)
       toast.success({
         title: 'リモートリポジトリを作成しました',
         description: `remoteName: ${remoteBranch}`,
-        isClosable: true
+        isClosable: true,
       })
+
+      // あとはローカルブランチをリモートに反映させていく
+      pushBranches(newRepositoryNode)
+      return
     }
+
+
+    // 存在する場合はそのままpushする
+    pushBranches(foundRemoteRepositoryNode)
+  }
+
+  const pushBranches = (newRepositoryNode: Node) => {
+    // リモートリポジトリに反映させていく
+
+    const nodes = reactFlowInstance.getNodes()
+    const localNodes = nodes.filter((node) => node.extent == undefined)
+    const remoteNodes = localNodes.map((localNode) => {
+      // ローカルノードを複製してリモートノードにしていく
+      const remoteNode = localNode
+      remoteNode.id = `${newRepositoryNode.id}-${localNode.id}`
+      remoteNode.extent = 'parent'
+      remoteNode.parentNode = newRepositoryNode.id
+      return remoteNode
+    })
+
+    reactFlowInstance.addNodes(remoteNodes)
+
+    // edgeも加えていく
+    const edges = reactFlowInstance.getEdges()
+    const localEdges = edges.filter((edge) => edge.data?.remoteName != newRepositoryNode.id)
+    const remoteEdges = localEdges.map((localEdge) => {
+      const remoteEdge = localEdge
+      remoteEdge.id = `${newRepositoryNode.id}-${localEdge.id}`
+      remoteEdge.source = `${newRepositoryNode.id}-${localEdge.source}`
+      remoteEdge.target = `${newRepositoryNode.id}-${localEdge.target}`
+      return remoteEdge
+    }) 
+
+    reactFlowInstance.addEdges(remoteEdges)
+
+    toast.success({
+      title: 'リモートリポジトリへpushしました。',
+      // description: `edges: ${JSON.stringify(remoteEdges)}`,
+      // description: `first remote branch id: ${JSON.stringify(remoteNodes)}}`,
+      isClosable: true,
+    })
   }
 
   const findRemoteRepositoryRegion = (
@@ -52,8 +99,10 @@ export default function usePushAction() {
 
   const createRemoteRepositoryRegion = (remoteName: string) => {
     // remoteNameがすでに存在していればこれは無視する
-    const region = createRemoteRepositoryNode(remoteName, { x: 200, y: 0})
+    // TODO: ここのx, yの値は外から渡すか、もしくはこのhooks内部で管理できる様にする
+    const region = createRemoteRepositoryNode(remoteName, { x: 200, y: 0 })
     reactFlowInstance.addNodes(region)
+    return region
   }
 
   const matchPushAction = (inputStr: string) => {
